c912c3ee3af316dcd3f23927493afb8b9f5152af
Connor La Rocca <connorlarocca@hotmail.com>
2022-10-25 15:13:19 -0400

    added sales pitches for the cars

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,9 @@
 car1 = { make: "Honda", model: "Accord", color: "blue", price: 25000 }
 car2 = { :make => "Hyundai", :model => "Kona", :color => "black", :price => 22000 }
 car3 = { make: "Nissan", model: "Ultima", color: "red", price: 28000 }
+
+puts "Our first car on the lot is a BEEEEEEAUTIFUL #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
+
+puts "The next car on the lot is a DROP DEEEEAD GORGEOUS #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
+
+puts "The last car on the lot today is a car that won't last long! #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
