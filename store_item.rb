32899d11b2b9d827871b232e80a6f4bf7eb81a68
Connor La Rocca <connorlarocca@hotmail.com>
2022-10-25 15:04:54 -0400

    makes list of cars

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+car1 = { make: "Honda", model: "Accord", color: "blue", price: 25000 }
+car2 = { :make => "Hyundai", :model => "Kona", :color => "black", :price => 22000 }
+car3 = { make: "Nissan", model: "Ultima", color: "red", price: 28000 }
