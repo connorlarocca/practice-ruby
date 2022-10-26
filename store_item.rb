ca96a46676501861a5ec7fb3d294c8da7ad897e1
Connor La Rocca <connorlarocca@hotmail.com>
2022-10-26 14:10:06 -0400

    DRY'd up the code and also sell food..?

--- store_item.rb
+++ store_item.rb
@@ -1,13 +1,3 @@
-# car1 = { make: "Honda", model: "Accord", color: "blue", price: 25000 }
-# car2 = { :make => "Hyundai", :model => "Kona", :color => "black", :price => 22000 }
-# car3 = { make: "Nissan", model: "Ultima", color: "red", price: 28000 }
-
-# puts "Our first car on the lot is a BEEEEEEAUTIFUL #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
-
-# puts "The next car on the lot is a DROP DEEEEAD GORGEOUS #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
-
-# puts "The last car on the lot today is a car that won't last long! #{car1[:make]} #{car1[:model]}. This #{car1[:color]} #{car1[:make]} can be yours for only #{car1[:price]}!"
-
 class Car
   attr_reader :make, :model, :color, :price
   attr_writer :price
@@ -18,33 +8,24 @@ class Car
     @color = input_options[:color]
     @price = input_options[:price]
   end
+end
 
-  # def make
-  #   @make
-  # end
+class Food < Car # THIS PLACE ALSO SELLS FOOD OK? IT'S A TEST PROBLEM!
+  attr_reader :shelflife
 
-  # def model
-  #   @model
-  # end
-
-  # def color
-  #   @color
-  # end
-
-  # def price
-  #   @price
-  # end
-
-  # def price=(input_price)
-  #   @price = input_price
-  # end
+  def initialize(input_options)
+    super
+    @shelflife = input_options[:shelflife]
+  end
 end
 
 car1 = Car.new(make: "Honda", model: "Accord", color: "blue", price: 25000)
-# car2 = Car.new("Hyundai", "Kona", "black", 22000)
-# car3 = Car.new("Nissan", "Ultima", "red", 28000)
+car2 = Car.new(make: "Hyundai", model: "Kona", color: "black", price: 22000)
+car3 = Car.new(make: "Nissan", model: "Ultima", color: "red", price: 28000)
+food1 = Food.new(make: "The Ground", model: "Peanut", color: "tan", price: 1, shelflife: "7 days")
 puts car1.make
 puts car1.model
 puts car1.color
 car1.price = 27000
 puts car1.price
+pp food1.shelflife
